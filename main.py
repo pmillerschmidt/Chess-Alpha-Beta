5ab20585224a22d6a3f1e4aee5d0146a21540488
Omeed Fallahi <omeed.fallahi@gmail.com>
2022-12-20 17:21:16 -0500

    command line args to choose player1 and player2 type

--- main.py
+++ main.py
@@ -14,23 +14,37 @@ def read_args():
     Process command line arguments
     """
     parser = argparse.ArgumentParser(description = "Process arguments for Blotto")
+    parser.add_argument('--P1', type = str, required = True, choices = ["greedy", "random", "minimax", "scout"])
+    parser.add_argument('--P2', type = str, required = True, choices = ["greedy", "random", "minimax", "scout"])
     parser.add_argument('--games', type = int, required = True)
     parser.add_argument('--viz', action = 'store_true', default = False)
     args = parser.parse_args()
 
+    P1 = args.P1
+    P2 = args.P2
     num_games = args.games
     visualize = args.viz
-    return (num_games, visualize)
+    return (P1, P2, num_games, visualize)
+
+def assign_player(player_color, player_type):
+    if player_type == "greedy":
+        return GreedyAgent(player_color, "gm2001.bin")
+    if player_type == "random":
+        return RandomAgent(player_color, "gm2001.bin")
+    if player_type == "minimax":
+        return MinimaxAgent(player_color, 3, "gm2001.bin")
+    if player_type == "scout":
+        return ScoutAgent(player_color, 3, "gm2001.bin")
 
 def main():
     """
     Driver function for main program execution
     """
-    num_games, visualize = read_args()
+    P1, P2, num_games, visualize = read_args()
 
     driver = Driver()
-    P1 = ScoutAgent(chess.WHITE, 3, "gm2001.bin")
-    P2 = GreedyAgent(chess.BLACK, 3,  "gm2001.bin")
+    P1 = assign_player(chess.WHITE, P1)
+    P2 = assign_player(chess.BLACK, P2)
 
     if num_games == 1: print(f"Playing {1} game...")
     else: print(f"Playing {num_games} games...")
